4dfa55da22b5bd0d1ae22174be10237017ae1abb
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2021-05-27 16:22:53 +0800

    Reorganise file structure and rewrite test scripts

---
+++ Energy_Subsystem/Initial Battery Discharge Profiles/SOC_battery1_static_SOH.m
@@ -0,0 +1,196 @@
+clear; close all;
+
+import = readmatrix('BATCYCLE3.CSV');
+
+% Arbitrary decisions
+    % Charge: Calibrate SoC with voltage lookup at above 3400V;
+    % Discharge: Calibrate SoC with voltage Lookup at below 3200V;
+    
+% Overall algorithm
+    % Determine upper and lower thresholds for OCV estimation, with values
+    % initialised at
+        % d_ocv_l = 3100;
+        % d_ocv_u = 3200;
+        % c_ocv_u = 3400;
+        % c_ocv_l = 3300;
+    % Coulomb counting in between thresholds, OCV below, above thresholds
+
+%% Seperate Plots
+state = import(:,1);
+voltage = import(:,2);
+current_ref = import(:,3);
+current = import(:,4);
+length = size(import, 1);
+
+% Indices indicating start and end of discharge cycles
+ds = 0; % discharge start
+de = 0; % discharge end
+cs = 0; % charge start
+ce = 0; % charge end
+discharged = 0;
+
+d_ocv_l = 3100;
+d_ocv_u = 3200;
+c_ocv_u = 3400;
+c_ocv_l = 3300;
+
+actual_charge = 0;
+
+% Identify start and end of discharge, as well as thresholds for OCV
+% Also track actual charge
+for i = 1:length
+    if(state(i,1) == 3)
+        actual_charge = actual_charge + 0.25;
+    end
+    if(state(i,1) == 3 && state(i-1) == 2)
+       ds = i;
+    end
+    if (state(i,1) == 4 && state(i-1) == 3)
+        de = i;
+        discharged = 1;
+    end
+    if (state(i,1) == 1  && state(i-1) == 4 && discharged == 1)
+        cs = i;
+    end
+    % Note: define state 6 for constant voltage charging (after state 1)
+    if (state(i,1) == 2  && (state(i-1) == 1 || state(i-1) == 6) && discharged == 1)
+        ce = i;
+        break;
+    end
+end
+
+discharge_time = de-ds+1;
+charge_time = ce-cs+1;
+    
+discharge_table = import(ds:de,2);
+discharge_SOC = (linspace(1,0,discharge_time))';
+discharge_SOC_MA = movmean(discharge_SOC, [100 0]);
+discharge_MA = movmean(discharge_table, [300 0]); % moving average
+dv_dt = diff(discharge_MA);
+discharge_map = [discharge_table, discharge_SOC, discharge_MA, [0;dv_dt]];
+
+charge_table = import(cs:ce,2);
+charge_SOC = (linspace(0,1,charge_time))';
+charge_SOC_MA = movmean(charge_SOC, [100 0]);
+charge_MA = movmean(charge_table, [300 0]); % moving average
+dv_ct = diff(charge_MA);
+charge_map = [charge_table, charge_SOC, charge_MA, [0;dv_ct]];
+
+% Identify thresholds for OCV
+for i = 1:size(dv_dt,1) % upper discharge OCV
+    if(dv_dt(i,1) > -0.12 && discharge_MA(i,1) > 3200)
+        d_ocv_u = discharge_MA(i,1);
+        break;
+    end
+end
+for i = 1:size(dv_dt,1) % lower discharge OCV
+    if(dv_dt(i,1) < -0.08 && discharge_MA(i,1) < 3100)
+        d_ocv_l = discharge_MA(i,1);
+        break;
+    end
+end
+for i = 1:size(dv_ct,1) % lower charge OCV
+    if(dv_ct(i,1) < 0.12 && charge_MA(i,1) < 3300)
+        c_ocv_l = charge_MA(i,1);
+        break;
+    end
+end
+for i = 1:size(dv_ct,1) % upper charge OCV
+    if(dv_ct(i,1) > 0.1 && charge_MA(i,1) > 3400)
+        c_ocv_u = charge_MA(i,1);
+        break;
+    end
+end
+
+
+%% Plot V vs SoC curves
+
+figure(1);
+plot (charge_SOC*100, charge_table, 'r.'); hold on;
+plot (charge_SOC*100, charge_MA, 'r-'); hold on;
+plot (discharge_SOC*100, discharge_table, 'b.'); hold on;
+plot (discharge_SOC*100, discharge_MA, 'b-'); hold on;
+xlabel('SOC (%)');
+ylabel('Terminal Voltage (mV)');
+legend('Charge','Discharge');
+title('Charging and Discharging Curves');
+
+%% Iterate through and determine total charge. Apply this to the first iteration as well.
+
+SOC = zeros(length,1);
+SOC_cc = zeros(length,1);
+nominal_charge = 1876;
+
+%% This is assuming full charge and discharge 
+
+% To ensure that charge is correct for first discharge
+
+for i = 1:length
+    if (i == 1)
+        SOC(i,1) = 0;
+    elseif (state(i,1) == 1 && state(i-1,1) == 0) % First valid term
+        [c, index] = min(abs(charge_table-voltage(i,1)));
+        c0 = charge_SOC_MA(index);
+        SOC(i,1) = c0;
+    elseif (state(i,1) == 5 || state(i,1) == 0) % freeze SOC
+        SOC(i,1) = SOC(i-1,1); 
+    elseif (state(i-1,1) == 0) % calibrate initial SoC with voltage lookup
+        [c, index] = min(abs(charge_table-voltage(i,1)));
+        SOC(i,1) = charge_SOC_MA(index);
+    elseif (state(i,1) == 1 || state(i,1) == 6) % charge
+        if (voltage(i,1) < c_ocv_l) % below threshold for OCV?
+            [c, index] = min(abs(charge_table-voltage(i,1)));
+            SOC(i,1) = charge_SOC_MA(index);
+        elseif (voltage(i,1) > c_ocv_u) % reach upper threshold for OCV?
+            [c, index] = min(abs(charge_table-voltage(i,1)));
+            movavg = (movmean([SOC(1:i-1); charge_SOC_MA(index)], [10 0])); % moving avg
+            SOC(i,1) = movavg(end);
+        else
+            SOC(i,1) = SOC(i-1,1)+ 0.25/nominal_charge;
+            SOC_cc(i,1) = SOC(i,1); % track the coulomb counting values
+        end
+    elseif (state(i,1) == 2) % charge rest
+        SOC(i,1) = 1;
+    elseif (state(i,1) == 3) % discharge
+        if (voltage(i,1) < d_ocv_l || voltage(i,1) > d_ocv_u || SOC(i-1,1) < 0.2)
+            [c, index] = min(abs(discharge_table-voltage(i,1)));
+            movavg = (movmean([SOC(1:i-1); discharge_SOC_MA(index)], [10 0])); % moving avg
+            SOC(i,1) = movavg(end);
+        else
+            SOC(i,1) = SOC(i-1,1) - 0.25/nominal_charge;            
+            SOC_cc(i,1) = SOC(i,1);  % track the coulomb counting values
+        end      
+    elseif (state(i,1) == 4) % discharge rest
+        SOC(i,1) = 0;
+    end
+end
+
+%% Plots
+% each time interval is 1s. convenient!
+
+figure(2);
+subplot(4,1,1);
+plot(state);
+xlabel('Time(s)');
+ylabel('State');
+title('Battery Characteristic');
+
+subplot(4,1,2);
+plot(voltage);
+xlabel('Time(s)');
+ylabel('Voltage (mV)');
+
+subplot(4,1,3);
+plot(current_ref, 'r'); hold on;
+plot(current,'b');
+xlabel('Time(s)');
+ylabel('Current (mA)');
+legend('Reference', 'Actual');
+
+subplot(4,1,4);
+plot(SOC*100); hold on;
+plot(SOC_cc*100);
+legend('SOC', 'SOC Coulomb Count');
+xlabel('Time(s)');
+ylabel('SOC (%)');
+ylim([-20 120]);
---
+++ Energy_Subsystem/Test Scripts/PnO_Attempt_Implementation/PnO_Attempt_Implementation.ino
@@ -0,0 +1,283 @@
+// ATTEMPT TO IMPLEMENT PnO Algorithm for MPPT
+// V/I Limit: 5V, 230mA
+// Perturb and Observe Algorithm
+// LED is ON when the PV panel is supplying power.
+
+//Packages
+#include <Wire.h>
+#include <INA219_WE.h>
+#include <SPI.h>
+#include <SD.h>
+INA219_WE ina219; // this is the instantiation of the library for the current sensor
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+const int chipSelect = 10; // SD
+
+// rest timer during charge and discharge rest
+unsigned int rest_timer;
+
+// triggers the fast (1kHz) loop. int_count = 1000 runs slow (1Hz) loop.
+unsigned int loop_trigger;
+unsigned int int_count = 0; // a variables to count the interrupts. Used for program debugging. 
+
+// Voltage PID Controller Stuff
+float ev=0,cv=0,ei=0; //internal signals // FIXME:
+  // ev: difference between V_ref and V_b
+  // cv: current obtained from voltage PID controller. need to saturate it
+  // ei: difference between desired and inductor current. error_amps in this case? FIXME:
+float kpv=0.05024,kiv=15.78,kdv=0; // voltage pid.
+float u0v,u1v,delta_uv,e0v,e1v,e2v; // u->output; e->error; 0->this time; 1->last time; 2->last last time
+float uv_max=4, uv_min=0; //anti-windup limitation
+
+// PnO Algorithm (only updates in slow loop)
+float vref = 2500;
+float v0, v1; // current and previous voltage values
+float p0, p1; // current and previous power values
+float i0, i1; // current and previous current values
+float p_diff, v_diff;
+
+// Current PID Controller Stuff
+float u0i, u1i, delta_ui, e0i, e1i, e2i; // Internal values for the current controller
+float ui_max = 1, ui_min = 0; //anti-windup limitation
+float kpi = 0.02512, kii = 39.4, kdi = 0; // current pid.
+float Ts = 0.001; //1 kHz control frequency.
+
+float current_measure; // obtained from ina219 (inductor current)
+float current_ref = 0, error_amps; // Current Control
+float pwm_out;
+float closed_pwm;
+float V_pv; // voltage at terminal VB
+boolean input_switch; // OLCL switch. 0 means back to IDLE
+
+// Panel Limits
+float current_limit = 0.2;
+float V_limit = 4700;
+
+// State Machine
+int state_num=0,next_state;
+String dataString;
+
+void setup() {
+  //Some General Setup Stuff
+
+  Wire.begin(); // We need this for the i2c comms for the current sensor
+  Wire.setClock(700000); // set the comms speed for i2c
+  ina219.init(); // this initiates the current sensor
+  Serial.begin(9600); // USB Communications
+
+  //Check for the SD Card - Initiate a "MPPT_PnO.csv" upon reset
+  Serial.println("\nInitializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("* is a card inserted?");
+    while (true) {} //It will stick here FOREVER if no SD is in on boot
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+  if (SD.exists("MPPT_PnO.csv")) { // Wipe the datalog when starting
+    SD.remove("MPPT_PnO.csv");
+  }
+  
+  noInterrupts(); //disable all interrupts
+  analogReference(EXTERNAL); // We are using an external analogue reference for the ADC
+
+  //SMPS Pins
+  pinMode(13, OUTPUT); // Using the LED on Pin D13 to indicate status
+
+  // TODO: Reassign these pins
+  pinMode(2, INPUT_PULLUP); // Pin 2 is the input from the CL/OL switch
+  pinMode(6, OUTPUT); // This is the PWM Pin
+
+  //LEDs on pin 7 and 8
+  pinMode(7, OUTPUT); // Error: Red Light
+  pinMode(8, OUTPUT); // Performing PnO Algorithm: Yellow
+
+  //Analogue input, currently the battery voltage (also port B voltage)
+  // TODO: Do we want to connect the solar PV panel in boost or or in Buck? Boost would be more convenient
+  pinMode(A0, INPUT);
+
+  // TimerA0 initialization for 1kHz control-loop interrupt.
+  TCA0.SINGLE.PER = 999; //
+  TCA0.SINGLE.CMP1 = 999; //
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc | TCA_SINGLE_ENABLE_bm; //16 prescaler, 1M.
+  TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP1_bm;
+
+  // TimerB0 initialization for PWM output
+  TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm; //62.5kHz
+
+  interrupts();  //enable interrupts.
+  analogWrite(6, 120); //just a default state to start with
+
+}
+
+void loop() {
+
+  // FAST LOOP (1kHZ)
+  if (loop_trigger == 1){
+      state_num = next_state; //state transition
+      V_pv = analogRead(A0)*4.096/1.03; //check the battery voltage (1.03 is a correction for measurement error, you need to check this works for you)
+      if (V_pv > 4700) { //Checking for Error states (just battery voltage for now) //TODO: adjust value for one PV panel
+          state_num = 5; //go directly to jail
+          next_state = 5; // stay in jail
+          digitalWrite(7,true); //turn on the red LED
+      }
+      current_measure = (ina219.getCurrent_mA()); // sample the inductor current (via the sensor chip)
+
+      // Voltage then Current PID -  Controllers calculate using V&A (not mV&mA)
+      ev = (vref - V_pv)/1000;  //voltage error at this time
+      closed_pwm = pidv(ev);  //voltage pid
+      // cv = saturation(cv, current_limit, 0); //current demand saturation
+      // ei = (cv - current_measure)/1000; //current error
+      // closed_pwm = pidi(ei);  //current pid
+      closed_pwm = saturation(closed_pwm, 0.99, 0.01);  //duty_cycle saturation
+      analogWrite(6, (int)(255 - closed_pwm * 255)); // write it out (inverting for the Buck here) //Not TODO: PWM Modulate Function
+
+      // Update Flags
+      int_count++; //count how many interrupts since this was last reset to zero
+      loop_trigger = 0; //reset the trigger and move on with life
+  }
+  
+  // SLOW LOOP (1Hz)
+  if (int_count == 1000) { 
+    input_switch = digitalRead(2); //get the OL/CL switch status
+    switch (state_num) { // STATE MACHINE (see diagram)
+      case 0:{ // Start state (no current, no LEDs)
+        vref = 2500;
+        if (input_switch == 1) { // if switch, move to charge
+          // First time, so reset voltage panel values
+          v1 = V_pv;
+          i1 = current_measure;
+          p1 = v1*i1;
+          next_state = 1;
+          digitalWrite(8,true);
+        } else { // otherwise stay put
+          next_state = 0;
+          digitalWrite(8,false);
+        }
+
+        break;
+      }
+      case 1:{ // Running state (a green LED)
+        p0 = V_pv * current_measure;
+        p_diff = p0-p1;
+        v0 = V_pv;
+
+        if (((p0>p1) && (v0>v1) || (p0<p1) && (v0<v1)) && (vref+100<V_limit)) {
+          vref = vref + 100;
+        } else if ((p0<p1) && (v0>v1) || (p0>p1) && (v0<v1)) {
+          vref = vref - 100;
+        } else {
+          Serial.println("Not incrementing or decrementing");
+        }
+
+        // Reset Flags for next iteration
+        next_state = 1;
+        digitalWrite(8,true); 
+        p1 = p0;
+        v1 = v0;
+
+        if(input_switch == 0){ // UNLESS the switch = 0, then go back to start
+          next_state = 0;
+          digitalWrite(8,false);
+        }
+        break;
+      }
+      case 5: { // ERROR state RED led and no current
+        next_state = 5; // Always stay here
+        digitalWrite(7,true);
+        digitalWrite(8,false);
+        if(input_switch == 0){ //UNLESS the switch = 0, then go back to start
+          next_state = 0;
+          digitalWrite(7,false);
+        }
+        break;
+      }
+
+      default :{ // Should not end up here ....
+        Serial.println("Boob");
+        next_state = 5; // So if we are here, we go to error
+        digitalWrite(7,true);
+      }
+      
+    }
+    
+    // Print values to serial monitor and csv
+    // State number, PV Voltage Reference(V), PV Voltage(V), PV Power(W)
+    dataString = String(state_num) + "," + String(vref/1000) + "," + String(V_pv/1000) + "," + String(current_measure/1000) + "," + String(p0/1000000); //build a datastring for the CSV file
+    Serial.println(dataString); // send it to serial as well in case a computer is connected
+
+    File dataFile = SD.open("MPPT_PnO.csv", FILE_WRITE); // open our CSV file
+    if (dataFile){ //If we succeeded (usually this fails if the SD card is out)
+      dataFile.println(dataString); // print the data
+    } else {
+      Serial.println("File not open"); //otherwise print an error
+    }
+    dataFile.close(); // close the file
+    int_count = 0; // reset the interrupt count so we dont come back here for 1000ms
+  }
+}
+
+// Timer A CMP1 interrupt. Every 1000us the program enters this interrupt. This is the fast 1kHz loop
+ISR(TCA0_CMP1_vect) {
+  loop_trigger = 1; //trigger the loop when we are back in normal flow
+  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP1_bm; //clear interrupt flag
+}
+
+float saturation( float sat_input, float uplim, float lowlim) { // Saturation function
+  if (sat_input > uplim) sat_input = uplim;
+  else if (sat_input < lowlim ) sat_input = lowlim;
+  else;
+  return sat_input;
+}
+
+// Current PID Controller - Not Needed for PnO
+float pidi(float pid_input) { // discrete PID function
+  float e_integration;
+  e0i = pid_input;
+  e_integration = e0i;
+
+  //anti-windup
+  if (u1i >= ui_max) {
+    e_integration = 0;
+  } else if (u1i <= ui_min) {
+    e_integration = 0;
+  }
+
+  delta_ui = kpi * (e0i - e1i) + kii * Ts * e_integration + kdi / Ts * (e0i - 2 * e1i + e2i); //incremental PID programming avoids integrations.
+  u0i = u1i + delta_ui;  //this time's control output
+
+  //output limitation
+  saturation(u0i, ui_max, ui_min);
+
+  u1i = u0i; //update last time's control output
+  e2i = e1i; //update last last time's error
+  e1i = e0i; // update last time's error
+  return u0i;
+}
+
+// Voltage PID Controller
+float pidv( float pid_input){
+  float e_integration;
+  e0v = pid_input;
+  e_integration = e0v;
+ 
+  //anti-windup, if last-time pid output reaches the limitation, this time there won't be any intergrations.
+  if(u1v >= uv_max) {
+    e_integration = 0;
+  } else if (u1v <= uv_min) {
+    e_integration = 0;
+  }
+
+  delta_uv = kpv*(e0v-e1v) + kiv*Ts*e_integration + kdv/Ts*(e0v-2*e1v+e2v); //incremental PID programming avoids integrations.there is another PID program called positional PID.
+  u0v = u1v + delta_uv;  //this time's control output
+
+  //output limitation
+  saturation(u0v,uv_max,uv_min);
+  
+  u1v = u0v; //update last time's control output
+  e2v = e1v; //update last last time's error
+  e1v = e0v; // update last time's error
+  return u0v;
+}
