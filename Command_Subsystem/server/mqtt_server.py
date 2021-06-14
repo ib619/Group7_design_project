0bf69572fcd1ff77bdb040aff079f8f4afb75d20
Zhao Siting <zhaositing1026@gmail.com>
2021-06-14 15:35:22 +0100

    trying to implement path again :p

--- Command_Subsystem/server/mqtt_server.py
+++ Command_Subsystem/server/mqtt_server.py
@@ -103,7 +103,8 @@ class MqttServer:
     def on_message_position(self, client, userdata, msg):
         db = create_connection('db/marsrover.db')
         if msg.topic == 'position/update':
-            if self.counter == 10:
+            logging.debug(self.counter)
+            if self.counter == 5:
                 # only add to db if 10th position update
                 data = str(msg.payload.decode("utf-8", "ignore"))
                 data = json.loads(data) # decode string into json format
